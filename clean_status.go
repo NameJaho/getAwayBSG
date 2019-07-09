6f0f000eedab47fc874df40203e804e4180303c0
jiangwei <jiangwei@mafengwo.com>
2019-07-09 16:04:00 +0800

    update

--- clean_status.go
+++ clean_status.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"getAwayBSG/configs"
 	"getAwayBSG/db"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -25,12 +31,41 @@ func main() {
 
 	if choice == 1 {
 		db.SetLianjiaStatus(0)
+		clean_visit()
 		fmt.Println("Done!")
 	} else if choice == 2 {
 		db.SetZhilianStatus(0, 0)
+		clean_visit()
 		fmt.Println("Done!")
 	} else {
+		clean_visit()
 		fmt.Println("选择错误！")
+	}
+
+}
+
+func clean_visit() {
+	conf := configs.Config()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, _ := mongo.NewClient(options.Client().ApplyURI(conf["dburl"].(string) + "/colly"))
+	if err := client.Connect(ctx); err != nil {
+		fmt.Println(err)
+	}
+
+	odb := client.Database("colly")
+	cookies := odb.Collection("colly_cookies")
+	visit := odb.Collection("colly_visited")
+	//清除全部的cookies
+	_, err := cookies.DeleteMany(ctx, bson.M{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = visit.DeleteMany(ctx, bson.M{})
+	if err != nil {
+		fmt.Println(err)
 	}
 
 }
